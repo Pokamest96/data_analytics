6372e151a272a1cb69cd74f368fa765a944082b1
Pokamest96 <aleksandr.pokamestov@mail.ru>
2018-11-12 22:46:31 +0300

    Create hw2.sql

---
+++ hw2.sql
@@ -0,0 +1,15 @@
+SELECT 'ФИО: Покаместов Александр Игоревич';
+-- 1.1
+SELECT * FROM ratings LIMIT 10;
+-- 1.2
+SELECT * FROM links WHERE imdbid LIKE '%42' AND movieid BETWEEN 100 AND 1000 LIMIT 10;
+-- 2.1
+SELECT IMDBID FROM LINKS INNER JOIN ratings ON links.movieid=ratings.movieid WHERE ratings.rating=5 LIMIT 10;
+-- 3.1
+SELECT COUNT(*) FROM LINKS LEFT JOIN ratings ON links.movieid=ratings.movieid WHERE ratings.rating IS NULL;
+-- 3.2
+SELECT userid, avg(rating) AS average_rating FROM ratings GROUP BY userid having AVG(rating) > 3.5 ORDER BY 2 DESC LIMIT 10;
+-- 4.1
+SELECT imdbid FROM links LEFT JOIN ratings ON links.movieid=ratings.movieid WHERE AVG(ratings.rating) >3.5 LIMIT 10;
+-- 4.2
+SELECT AVG(rating) FROM ratings WHERE userid IN (SELECT userid FROM ratings GROUP BY userid HAVING count(*)>10);
